23960779a10fce42c15aaf30f953d898463e7b5c
Grant Archibald <31553604+Grant-Archibald-MS@users.noreply.github.com>
2023-09-18 15:49:14 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# playwright-merge-pages
-Merge web pages together into a single pdf document
+# Playwright Web Page Merge
+
+Merge web pages together into a single pdf document. This code sample make use of your interactive Microsoft Edge browser to saves pages into a single pdf file with an optional cover page.
+
+## Notes
+
+Some notes and assumptions
+
+1. As Microsoft Edge browser is used pages behind a security auth prompt can be navigated to and saved.
+
+2. The app assumes tha the pages to be saved use a base URL set in App.config
+
+3. Uses interactive mode for any login requirements
+
+4. Assume Microsoft Edge is installed in default user profile on Windows
+
+5. Visits the home page first to allow for login. Following pages will used login browser credentials
+
+7. .Net 7.0 SDK of greater is installed 
+
+## Getting Started
+
+1. Ensure that you have the [Microsoft .Net SDK Installed](https://dotnet.microsoft.com/en-us/download/visual-studio-sdks)
+
+2. Ensure that the installed SDK is in your path
+
+3. Edit the App.config with the the site and pages to combine
+
+|Setting|Description|
+|-----=-|-----------|
+|BaseUrl|The web page with {0} wildcard to visit
+|Home|The first page to include in the merged document after the cover
+|Pages|A comma delimited list of pages to include
+|Cover|A pdf document to include as cover page
+
+4. Build the code
+
+```cmd
+cd src
+dotnet build    
+```
+
+5. Ensure Playwright is installed
+
+```cmd
+# Install required browsers - replace netX with actual output folder name, e.g. net6.0.
+pwsh bin/Debug/netX/playwright.ps1 install 
+``````
+
+4. Run the merge
+
+```cmd
+dotnet run
+```
---
+++ src/Program.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Playwright;
+using org.pdfclown.files;
+using System.Configuration;
+
+using var playwright = await Playwright.CreateAsync();
+var context = string.Format(@"C:\Users\{0}\AppData\Local\Microsoft\Edge\User Data\Default", Environment.UserName);
+await using var browser = await playwright.Chromium.LaunchPersistentContextAsync(context, new BrowserTypeLaunchPersistentContextOptions {
+    Channel = "msedge", 
+    Headless = false
+});
+
+var baseUrl = ConfigurationManager.AppSettings["BaseUrl"];
+var homePage = ConfigurationManager.AppSettings["Home"];
+var pages = ConfigurationManager.AppSettings["Pages"];
+var coverPage = ConfigurationManager.AppSettings["Cover"];
+
+var page = await browser.NewPageAsync();
+await page.GotoAsync(string.Format(baseUrl,homePage));
+var pdf = await page.PdfAsync(new PagePdfOptions() { DisplayHeaderFooter = false });
+System.IO.File.WriteAllBytes( "00-" + homePage + ".pdf", pdf);
+
+int counter = 0;
+foreach ( var pageName in pages.Split(',')) {
+    page = await browser.NewPageAsync();
+    await page.GotoAsync(string.Format(baseUrl,pageName));
+    await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+    
+    var match = page.Locator("img");
+    var images = await match.AllAsync();
+    if ( images.Count() > 0 ) {
+        foreach ( var image in images ) {
+            var src = await image.GetAttributeAsync("src");
+            if ( !string.IsNullOrEmpty(src) && src.StartsWith("media")) {
+                await image.EvaluateAsync(string.Format("node => node.src = \"{0}\"", string.Format(baseUrl, src)));
+            }
+        }
+    }
+    counter++;
+
+    pdf = await page.PdfAsync(new PagePdfOptions() { DisplayHeaderFooter = false, PreferCSSPageSize = true });
+    System.IO.File.WriteAllBytes( (100 + counter).ToString().Substring(1) + "-" + pageName + ".pdf", pdf);
+    await page.CloseAsync();
+}
+MergePdf(".", "merged.pdf", coverPage);
+
+static void MergePdf(string srcPath, string destFile, string cover)
+{
+    var list = Directory.GetFiles(Path.GetFullPath(srcPath));
+
+    if (string.IsNullOrWhiteSpace(srcPath) || string.IsNullOrWhiteSpace(destFile) || list.Length <= 1)
+        return;
+
+    var files = list
+        .Where(f => {
+                int result;
+                var name = Path.GetFileName(f);
+                return int.TryParse(name.Substring(0,1), out result);
+            })
+        .OrderBy(f => f)
+        .Select(System.IO.File.ReadAllBytes)
+        .ToList();
+
+    if ( files.Count == 0 ) {
+        Console.WriteLine("No matching files found");
+        return;
+    }
+
+    using (var dest = new org.pdfclown.files.File(new org.pdfclown.bytes.Buffer(System.IO.File.ReadAllBytes(cover))))
+    {
+        var document = dest.Document;
+        var builder = new org.pdfclown.tools.PageManager(document);
+        foreach (var file in files)
+        {
+            using (var src = new org.pdfclown.files.File(new org.pdfclown.bytes.Buffer(file)))
+            { 
+                builder.Add(src.Document);
+            }
+        }
+
+        dest.Save(destFile, SerializationModeEnum.Incremental);
+    }
+}
